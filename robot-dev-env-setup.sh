885e5caf50f61ab87ed3463e3db8d128f5e3b4ec
Simon Dietschi <simon.dietschi@bluecare.ch>
2015-08-28 16:07:57 +0200

    robot script updated

--- robot-dev-env-setup.sh
+++ robot-dev-env-setup.sh
@@ -9,11 +9,32 @@
 #
 #############################################################
 
+echo "====================================================="
+echo "Installation script to prepare a robot framework"
+echo "dev environment on OSX."
+echo ""
+echo "PLEASE NOTE: If nothing has been installed before,"
+echo "this script will install the latest package versions."
+echo ""
+echo "Run this script locally on your Mac!"
+echo "====================================================="
+echo ""
+read -p "Continue ? [Enter]"
+echo ""
+echo ""
+
+# Install latest Python via brew which install pip as well
+brew install python
+
 # Intstall robot framework
 pip install robotframework
 
-# Install selenium2lib
+# Install selenium2lib for web testing
 pip install robotframework-selenium2library
+
+# Install additional libraries
+pip install robotframework-sshlibrary
+pip install robotframework-requests
 
 # Install chrome and web driver
 brew cask install google-chrome
